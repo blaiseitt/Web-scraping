7b6384419c780db6f05272f167a0fc9d968584aa
blaiseitt <blazej.poniatowski@gmail.com>
2025-01-08 15:21:11 +0100

    Print multiple songs

--- JavaScrapper/src/main/java/pl/buarzej/SongScrapper.java
+++ JavaScrapper/src/main/java/pl/buarzej/SongScrapper.java
@@ -2,6 +2,7 @@ package pl.buarzej;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -23,21 +24,26 @@ public class SongScrapper {
             Elements songs = document.select("div.item.song.visible");
             //need to scroll page in order to retrieve more songs
             System.out.println("Number of songs retrieved: " + songs.size());
-            String authorAndTitle = songs.first().select("span.title-text").text();
 
-            //split author and title - do it in more convinient way
-            String authorAndTitleParts[] = authorAndTitle.split("-", 2);
-
-            String hour = songs.first().select("span.hour").text();
-            System.out.println("Artist: " + authorAndTitleParts[0].trim());
-            System.out.println("Song title: " + authorAndTitleParts[1].trim());
-            System.out.println("Time played: " + hour);
+            for (Element song: songs) {
+                printSongDetails(song);
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             driver.quit();
         }
+    }
 
+    private static void printSongDetails(Element song) {
+        String authorAndTitle = song.select("span.title-text").text();
+        //split author and title - do it in more convinient way, for example artist a-ha is bugged because of this solution
+        String authorAndTitleParts[] = authorAndTitle.split("-", 2);
+
+        String hour = song.select("span.hour").text();
+        System.out.println("Artist: " + authorAndTitleParts[0].trim());
+        System.out.println("Song title: " + authorAndTitleParts[1].trim());
+        System.out.println("Time played: " + hour);
     }
 }
