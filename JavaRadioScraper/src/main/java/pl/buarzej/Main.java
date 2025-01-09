e0ed7802b0f14414e9ad1c362fb9750d01a0e5e7
blaiseitt <blazej.poniatowski@gmail.com>
2025-01-09 03:40:11 +0100

    Added properties file and load them in main class - possible revert.

--- JavaRadioScraper/src/main/java/pl/buarzej/Main.java
+++ JavaRadioScraper/src/main/java/pl/buarzej/Main.java
@@ -4,13 +4,25 @@ import pl.buarzej.model.Song;
 import pl.buarzej.service.ScrapingService;
 import pl.buarzej.utils.PrintSongDataUtil;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Properties;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "E:\\Projects\\Webdrivers\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        Properties properties = new Properties();
+        try (InputStream input = Main.class.getClassLoader().getResourceAsStream("config.properties")) {
+            properties.load(input);
+        } catch (Exception e) {
+            System.out.println("Unable to find specified properties file");
+            e.printStackTrace();
+            return;
+        }
+
+        System.setProperty(properties.getProperty("system.chromedriver"), properties.getProperty("path.to.chromedriver"));
 
         ScrapingService service = new ScrapingService();
         List<Song> songs = service.runSelectedScrapers(List.of("plus"));
