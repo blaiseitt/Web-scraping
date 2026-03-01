7312027f678c591e83653a4c000bcb15e5be21f4
blaiseitt <blazej.poniatowski@gmail.com>
2026-03-01 22:14:12 +0100

    Some frontend changes

--- front-radio/src/app/app.component.ts
+++ front-radio/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppNavbar } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, AppNavbar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ front-radio/src/app/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+
+
+@Component({
+    selector: 'app-navbar',
+    standalone: true,
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss'],
+    imports: [MatIconModule, MatButtonModule, MatToolbarModule, FormsModule, MatFormFieldModule, MatInputModule]
+})
+
+ export class AppNavbar {
+
+}
