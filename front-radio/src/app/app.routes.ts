baa612f70ff5c7a0919912d18da5602e9adea2c6
blaiseitt <blazej.poniatowski@gmail.com>
2026-03-28 16:23:13 +0100

    Upgrade project structure

--- front-radio/src/app/app.routes.ts
+++ front-radio/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routeConfig: Routes = [
     {
